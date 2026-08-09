65caec44aa7d6ec38464e1302cf51a41ae69354e
scubamaster96 <mathydagonlaz57@gmail.com>
2026-08-09 16:18:47 +0300

    auto updates

--- Plugins/offlinechat.lua
+++
@@ -1,9 +0,0 @@
-function MenuManager:toggle_chatinput()
-	if Application:editor() or SystemInfo:platform() ~= Idstring("WIN32") or self:active_menu() or not managers.network:session() then
-		return
-	end
-	if managers.hud then
-		managers.hud:toggle_chatinput()
-		return true
-	end
-end
